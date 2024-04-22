a74c48165f6d581345dc1e11617fde69bbafdd92
Zhaoping <xmluozp@gmail.com>
2024-04-21 22:03:46 -0700

    update easypost

--- easypost_api_study/node_account/getting_start.js
+++ easypost_api_study/node_account/getting_start.js
@@ -1,8 +1,8 @@
 const Easypost = require('@easypost/api');
-const api = new Easypost('process.env.EZTK5d8422e7c1464a13a437dfa3f9d9705egFFDL0xruaG3uXanf488Yg');
+const api = new Easypost('EZTK5d8422e7c1464a13a437dfa3f9d9705egFFDL0xruaG3uXanf488Yg');
 
 const testFunction = async () => {
-  const fromAddress = await new api.Address({
+  const fromAddress = await api.Address.create({
       company: 'company1',
       street1: '1st Monkey Street',
       city: 'Winnipeg',
@@ -10,10 +10,9 @@ const testFunction = async () => {
       zip: 'R3T 5A2',
       phone: '204-456-7890',
   });
-
   console.log(fromAddress);
 
-  const toAddress = await new api.Address({
+  const toAddress = await api.Address.create({
     name: "Sue's home",
     company: "Sue's home",  //有些用户不是公司，自动同步name到company
     street1: '1 E 161st St.',
@@ -25,7 +24,7 @@ const testFunction = async () => {
   
   console.log(toAddress);
 
-  const parcel = await new api.Parcel({
+  const parcel = await api.Parcel.create({
     length: 9,
     width: 6,
     height: 2,
@@ -45,7 +44,7 @@ const testFunction = async () => {
   console.log(parcel);  
 **********************************************************************/
 
-const shipment = await new api.Shipment({         //这部分分为shipment object 和 rates object 具体需要再看
+const shipment = await api.Shipment.create({         //这部分分为shipment object 和 rates object 具体需要再看
   to_address: toAddress,            //发货人的地址，默认为 from_address，可以用ID代替eg.:from_address: { id: 'adr_...' },
   from_address: fromAddress,        //买家的地址，默认为 to_address，可以用ID代替eg.:to_address: { id: 'adr_...' },
   parcel: parcel,                   //parcel为包装的尺寸和重量的信息，可以用ID代替eg.:parcel: { id: 'prcl_...' },
