cb2ca628457a7ea19e863946266dc2d277845de3
WINNIAPPJUSTGO <150107125+WINNIAPPJUSTGO@users.noreply.github.com>
2024-04-25 16:30:18 -0500

    try push

--- easypost_api_study/node_account/create_a_user.js
+++ easypost_api_study/node_account/create_a_user.js
@@ -1,12 +1,14 @@
-const EasyPostClient = require('@easypost/api');
+const EasyPostClient = require("@easypost/api");
 
-const client = new EasyPostClient('EZTK5d8422e7c1464a13a437dfa3f9d9705egFFDL0xruaG3uXanf488Yg');
-//需要使用非测试的api key
+const client = new EasyPostClient(
+  "EZTK5d8422e7c1464a13a437dfa3f9d9705egFFDL0xruaG3uXanf488Yg"
+);
+//需要使用非测试的api key。
 const testFunction = async () => {
   const user = await client.User.create({
-    name: 'Test 1',
+    name: "Test 1",
   });
 
   console.log(user);
-}
-  testFunction();
+};
+testFunction();
